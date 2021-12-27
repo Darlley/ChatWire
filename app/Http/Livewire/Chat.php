bf7b07263bd20a1c468dd0b5a62767016223c7c9
Darlley <systembrtt@gmail.com>
2021-12-27 17:56:13 -0400

    chat not realtime

--- app/Http/Livewire/Chat.php
+++ app/Http/Livewire/Chat.php
@@ -8,7 +8,6 @@ use App\Models\{
     Message
 };
 use Livewire\Component;
-use App\Events\ChatStatusUpdated;
 
 class Chat extends Component
 {
@@ -18,7 +17,7 @@ class Chat extends Component
     public $data_messages = []; // array
 
     public User $logged_user;
-    public $to_user = ['id'=>'', 'name'=>''];
+    public $to_user = ['id'=> '', 'name'=>''];
     public $chat_user = [];
     public $list_users = [];
 
@@ -29,53 +28,58 @@ class Chat extends Component
 
     public function mount(User $user){
 
-        
         // To User
         $this->logged_user = User::with('senders.receipt')->find(Auth::id());
-        // $this->list_messages_bd = $this->logged_user->senders;
-        
-        // From User
-        // $this->chat_user = $user->load('profile')->toArray();
-        
+        // $this->list_messages_bd = $this->logged_user->senders; 
         // All Users
         $this->list_users = User::where('id','!=',Auth::id())->get();
+
     }
 
     public function mountUser($id,$chave){
+
         $this->reset('chat_user');
         $this->reset('data_messages');
-        
+        $this->reset('list_messages_bd');
+
         $this->to_user = [
             'id' => $id,
             'name' => User::find($id)['name']
         ];
-        foreach($this->logged_user->senders as $userChat){
-            if( $userChat['to_user_id'] == $this->list_users[$chave]['id'] ){
+
+        $teste = Message::all();
+        foreach($teste as $userChat){
+            if( ($userChat['from_user_id'] == Auth::id() || $userChat['to_user_id'] == Auth::id()) && ($userChat['from_user_id'] == $this->to_user['id'] || $userChat['to_user_id'] == $this->to_user['id']) ){
                 array_push($this->chat_user, $userChat);
             }
         }
         $this->list_messages_bd = $this->chat_user;
+
+        //     if( $userChat['to_user_id'] == $this->list_users[$chave]['id'] ){
+        //     }
+
     }
+
     public function disassembly(){
+
         $this->reset('to_user');
         $this->reset('chat_user');
         $this->reset('data_messages');
+
     }
 
     public function sendMessage(){
 
-        $data = [
-            'from_user_id' => $this->logged_user->id,
-            'to_user_id' => $this->to_user['id'],
-            'content' => $this->message
-        ];
-        if($data['content']){
-
+        if($this->message != ''){
+            $data = [
+                'from_user_id' => $this->logged_user->id,
+                'to_user_id' => $this->to_user['id'],
+                'content' => $this->message,
+                'created_at' => now()
+            ];
             array_push($this->data_messages, $data); // array ()
             Message::create($data); // database
             $this->reset('message');
-
         }
-
     }
 }
